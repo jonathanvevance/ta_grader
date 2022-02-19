a7d4649dd0eb6859cdd5232ac3d93e2d6ad618fb
Jonathan Ve Vance <jonathanvevance@gmail.com>
2022-02-19 10:24:34 +0530

    back button in question form added

--- setter/views.py
+++ setter/views.py
@@ -81,6 +81,7 @@ def add_question(request, pk):
         'title': 'Question',
         'navbar_link': None,
         'form': form,
+        'assignment_id': pk,
     }
     return render(request, "setter/question.html", context)
 
